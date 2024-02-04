119fa9ba6623aed4be286b0e42f5c5b2cea4335c
Stefan Pla <spla@icloud.com>
2024-02-04 15:24:45 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(arr,str) {
+    return arr.filter(name => name.toLowerCase() === str.toLowerCase());
+}
+
+function fuzzyMatch(arr,str) {
+    return arr.filter(name => name[0] === str[0])
+}
+
+function matchName(arr,str) {
+    return arr.filter(driver => driver.name === str);
+}
+
+//arr is an array of driver objects. each driver object has two properties, name and hometown
